be5bf109a30a2d0bebcf9f3aa604aaa084ee878a
yarin-code <yarinazulay555@gmail.com>
2026-03-30 17:29:12 +0300

    first hagasha

---
+++ src/User.java
@@ -0,0 +1,77 @@
+import java.util.regex.Pattern;
+
+public class User {
+
+    private static final String EMAIL_REGEX = "^(?=.{1,}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9][a-zA-Z0-9.-]*\\.[a-zA-Z]{2,}$";
+    private static final String PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#.,+^*&$%])[a-zA-Z0-9!@#.,+^*&$%]+$";
+
+    private static final Pattern emailPattern = Pattern.compile(EMAIL_REGEX);
+    private static final Pattern passwordPattern = Pattern.compile(PASSWORD_REGEX);
+
+    private String email;
+    private String password;
+
+
+
+    public User(String email, String password) throws Exception {
+
+        if (!emailPattern.matcher(email).matches()) {
+
+            throw new Exception(email + " " + password +" Please enter a valid Email as username");
+
+        }
+
+        if (!passwordPattern.matcher(password).matches()) {
+
+            throw new Exception(email + " " + password +" Please enter a valid password");
+
+        }
+
+
+        if(email.length() > 50) {
+
+            throw new Exception(email + " " + password +" Username is too long, try a shorter one");
+
+        }
+
+
+        if (password.length() < 8) {
+
+            throw new Exception(email + " " + password +" Your password is too short, add more characters");
+
+
+        }
+
+        if (password.length() > 12) {
+
+            throw new Exception(email + " " + password +" Your password is too long, try a shorter one");
+
+        }
+
+
+        this.email = email;
+        this.password = password;
+
+
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public String toString(){
+
+        return email + " " + password;
+
+
+    }
+
+
+}
---
+++ src/UserApp.java
@@ -0,0 +1,74 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class UserApp {
+    public static void main(String[] args) {
+
+
+        ArrayList<User> users = new ArrayList<User>();
+
+        try {
+
+            File myFile = new File("Users.txt");
+            Scanner reader = new Scanner(myFile);
+
+            while (reader.hasNextLine()) {
+                String email = reader.next();
+                String password = reader.next();
+
+                User newUser;
+
+                try {
+                    newUser = new User(email, password);
+                }
+                catch (Exception e) {
+
+                    System.out.println(e.getMessage());
+                    continue;
+
+                }
+
+                users.add(newUser);
+
+            }
+            reader.close();
+
+        }
+
+        catch (Exception e) {
+
+            System.out.println("File not found");
+
+        }
+
+        Collections.sort(users,(u1, u2) -> u1.getEmail().compareTo(u2.getEmail()));
+
+        for(int i = 0; i<users.size(); i++){
+            System.out.println(users.get(i));
+        }
+
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("out1.txt"))) {
+            for(int i = 0; i<users.size(); i++){
+                writer.write(String.valueOf(users.get(i)));
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error creating the file" + e.getMessage());
+        }
+
+
+
+
+    }
+
+
+}
+
+
+
